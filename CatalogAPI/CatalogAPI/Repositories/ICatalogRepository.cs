8f79d7794530524f1f7bb353c6d5d5d064f4b5a9
eswaribala <parameswaribala@gmail.com>
2022-05-31 17:55:35 +0530

    updated

---
+++ CatalogAPI/CatalogAPI/Repositories/ICatalogRepository.cs
@@ -0,0 +1,18 @@
+﻿using CatalogAPI.Models;
+
+namespace CatalogAPI.Repositories
+{
+    public interface ICatalogRepository
+    {
+       Task<Catalog> AddCatalog(Catalog Catalog);
+        Task<Catalog> UpdateCatalog(Catalog Catalog);
+        Task DeleteCatalog(Catalog Catalog);
+        Task<Catalog> GetCatalogById(long CatalogId);
+        Task<IEnumerable<Catalog>> GetAllCatalog();
+        
+
+
+
+
+    }
+}
