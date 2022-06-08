1528b2f11188571ad2dbe4596d3efc116db79c08
eswaribala <parameswaribala@gmail.com>
2022-06-08 14:23:34 +0530

    updated

---
+++ CamundaMoviePublisherAPI/CamundaMoviePublisherAPI/Handlers/MovieTaskHandler.cs
@@ -0,0 +1,13 @@
+﻿using Camunda.Worker;
+
+namespace CamundaMoviePublisherAPI.Handlers
+{
+    [HandlerTopics("movietask")]
+    public class MovieTaskHandler : IExternalTaskHandler
+    {
+        public Task<IExecutionResult> HandleAsync(ExternalTask externalTask, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
