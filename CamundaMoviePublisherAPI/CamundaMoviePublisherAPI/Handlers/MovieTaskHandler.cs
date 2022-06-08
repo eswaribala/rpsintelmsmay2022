f400c1c39504a18adc97b57ef93a07ba070b8b44
eswaribala <parameswaribala@gmail.com>
2022-06-08 14:59:24 +0530

    updated

--- CamundaMoviePublisherAPI/CamundaMoviePublisherAPI/Handlers/MovieTaskHandler.cs
+++ CamundaMoviePublisherAPI/CamundaMoviePublisherAPI/Handlers/MovieTaskHandler.cs
@@ -5,9 +5,30 @@ namespace CamundaMoviePublisherAPI.Handlers
     [HandlerTopics("movietask")]
     public class MovieTaskHandler : IExternalTaskHandler
     {
-        public Task<IExecutionResult> HandleAsync(ExternalTask externalTask, CancellationToken cancellationToken)
+        private readonly ILogger<MovieTaskHandler> _logger;
+        public MovieTaskHandler(ILogger<MovieTaskHandler> logger)
         {
-            throw new NotImplementedException();
+            _logger = logger;
+        }
+        public async Task<IExecutionResult> HandleAsync(ExternalTask externalTask, CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("Movie Data handler is called from Camunda...");
+            try
+            {
+                _logger.LogInformation($"Movie Data processing..........");
+                //Mimicking operation
+                Task.Delay(500).Wait();
+
+                _logger.LogInformation($"Movie Data processing, {externalTask.Variables["DirectorName"].Value} ");
+                return new CompleteResult();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"error occured!! error messge: {ex.Message}");
+                //return failure
+                return new BpmnErrorResult("Movie Data Processing Failure", 
+                    "Error occured while processing payment..");
+            }
         }
     }
 }
