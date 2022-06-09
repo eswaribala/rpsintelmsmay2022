655487476b682328d9155aaf2159dc10203dc6a2
eswaribala <parameswaribala@gmail.com>
2022-06-09 17:04:04 +0530

    updated

--- InventoryService/Startup.cs
+++ InventoryService/Startup.cs
@@ -13,7 +13,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
-
+using Prometheus;
 
 namespace InventoryService
 {
@@ -63,6 +63,7 @@ namespace InventoryService
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "InventoryService v1"));
             }
+            app.UseMetricServer();
 
             app.UseHttpsRedirection();
 
